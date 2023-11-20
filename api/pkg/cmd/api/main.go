0c1ec85a3b42b02898845fdd7e0be1b46c260acf
myantyuWorld <nepinepi984@gmail.com>
2023-11-20 16:14:33 +0900

    mysql接続処理をインフラ層に移設

---
+++ api/pkg/cmd/api/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"api/pkg/infrastructure/repository"
+	"log"
+	"os"
+
+	"github.com/labstack/echo/v4"
+)
+
+// Go言語：文法基礎まとめ | https://qiita.com/HiromuMasuda0228/items/65b9a593275f769f6b69
+func main() {
+	user := os.Getenv("MYSQL_USER")
+	pass := os.Getenv("MYSQL_PASSWORD")
+	host := os.Getenv("MYSQL_HOST")
+	dbname := os.Getenv("MYSQL_DATABASE")
+
+	dbCfg := repository.NewMySQLConfig(host, 3306, dbname, user, pass)
+	db, err := repository.ConnRDB(dbCfg)
+	//
+	// [参考]log.Fatalなどは、main.goなどプログラムエントリーでのみ使用する
+	//
+	// func Fatal(v ...any) {
+	// 	std.Output(2, fmt.Sprint(v...))
+	// 	os.Exit(1)
+	// }
+	if err != nil {
+		log.Fatal(err)
+	}
+	e := echo.New()
+
+	log.Printf("db :: %#v\n", db)
+
+	e.Logger.Fatal(e.Start(":8080"))
+}
---
+++ api/pkg/infrastructure/repository/connection.go
@@ -0,0 +1,86 @@
+package repository
+
+import (
+	"fmt"
+	"time"
+
+	driver "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type RDB struct {
+	*gorm.DB
+}
+
+func ConnRDB(cfg DBConfig) (*RDB, error) {
+	db, err := gorm.Open(cfg.Dialector(), &gorm.Config{})
+	if err != nil {
+		return nil, fmt.Errorf("failed to connect rdb: %w", err)
+	}
+
+	sql, err := db.DB()
+	if err != nil {
+		return nil, err
+	}
+
+	sql.SetConnMaxIdleTime(cfg.MaxIdleTime())
+	sql.SetConnMaxLifetime(cfg.MaxLifetime())
+	sql.SetMaxIdleConns(cfg.MaxIdle())
+	sql.SetMaxOpenConns(cfg.MaxOpen())
+
+	return &RDB{db}, nil
+}
+
+type DBConfig interface {
+	Dialector() gorm.Dialector
+	MaxIdleTime() time.Duration
+	MaxLifetime() time.Duration
+	MaxIdle() int
+	MaxOpen() int
+}
+
+func NewMySQLConfig(host string, port int, database, user, password string) DBConfig {
+	return &mySQLConfig{
+		Host:     host,
+		Port:     port,
+		Database: database,
+		User:     user,
+		Password: password,
+	}
+}
+
+type mySQLConfig struct {
+	Host     string
+	Port     int
+	Database string
+	User     string
+	Password string
+}
+
+func (cfg *mySQLConfig) Dialector() gorm.Dialector {
+	driverConfig := &driver.Config{
+		User:   cfg.User,
+		Passwd: cfg.Password,
+		Net:    "tcp",
+		Addr:   fmt.Sprintf("%s:%d", cfg.Host, cfg.Port),
+		DBName: cfg.Database,
+	}
+	return mysql.Open(driverConfig.FormatDSN())
+}
+
+func (cfg *mySQLConfig) MaxIdleTime() time.Duration {
+	return 0
+}
+
+func (cfg *mySQLConfig) MaxLifetime() time.Duration {
+	return 0
+}
+
+func (cfg *mySQLConfig) MaxIdle() int {
+	return 0
+}
+
+func (cfg *mySQLConfig) MaxOpen() int {
+	return 0
+}
