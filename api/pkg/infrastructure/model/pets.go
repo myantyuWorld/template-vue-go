7117ce0c30f00ea3658b139634fcc6cdb46b1391
myantyuWorld <nepinepi984@gmail.com>
2023-12-04 09:54:00 +0900

    modify property

--- api/pkg/infrastructure/model/pets.go
+++ api/pkg/infrastructure/model/pets.go
@@ -2,12 +2,28 @@ package dbmodel
 
 import "time"
 
+// insert into
+//
+//	pets(
+//	  id,
+//	  name,
+//	  birth_day,
+//	  sex,
+//	  now_weight,
+//	  target_weight,
+//	  created_at,
+//	  updated_at
+//	)
+//
+// values
+//
+//	(1, "なつ", now(), 1, 4000, 5000, now(), now());
 type Pets struct {
 	ID        uint64
 	Name      string
-	Birthday  time.Time
+	BirthDay  time.Time
 	CreatedAt time.Time
-	UpdateAt  time.Time
+	UpdatedAt time.Time
 }
 
 func (mdl *Pets) TableName() string {
