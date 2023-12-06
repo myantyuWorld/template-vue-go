6324f813676f626dcc0f84c8714bdb09d7d437df
myantyuWorld <nepinepi984@gmail.com>
2023-12-06 09:02:37 +0900

    Condition mock generate

--- api/pkg/domain/repository/condition_repository.go
+++ api/pkg/domain/repository/condition_repository.go
@@ -5,7 +5,9 @@ import (
 	dbmodel "api/pkg/infrastructure/model"
 )
 
+//go:generate mockgen -source=$GOFILE -package=mock_$GOPACKAGE -destination=../../mock/$GOPACKAGE/$GOFILE
 type ConditionRepository interface {
+	Post(db *infrastructure.RDB, petId uint64, condition *dbmodel.Conditions) (*dbmodel.Conditions, error)
 	Finds(db *infrastructure.RDB, petId uint64) ([]*dbmodel.Conditions, error)
 	FindNewest(db *infrastructure.RDB, petId uint64) (*dbmodel.Conditions, error)
 }
